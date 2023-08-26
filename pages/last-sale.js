79fbb909ed6d518d6e29fd5358b2ac1265fdcf6f
qauntico <bauketfabrice10@gmail.com>
2023-08-26 14:54:53 +0100

    learn to use the useSWR hook for data fetching

---
+++ pages/last-sale.js
@@ -0,0 +1,43 @@
+import { useEffect,useState } from "react";
+import  useSWR  from "swr";
+
+const fetcher = url => fetch(url).then(r => r.json())
+
+export default function LastSalesPage(){
+    const [sales, setSales] = useState([]);
+    const {data, error,isLoading} = useSWR('api',fetcher)
+    console.log(data)
+
+    useEffect(() => {
+        console.log(data)
+        if (data) {
+          const transformedSales = [];
+    
+          for (const key in data) {
+            transformedSales.push({
+              id: key,
+              username: data[key].username,
+              volume: data[key].volume,
+            });
+          }
+    
+          setSales(transformedSales);
+        }
+      }, [data]);
+
+    if(error){
+        return <p>Failed to load</p>
+    }
+
+    if(isLoading){
+        return <p>loading....</p>
+    }
+ 
+    return <ul>
+        {sales.map((sale) => (
+            <li key={sale.id}>
+                {sale.username}
+            </li>
+        ))}
+    </ul>
+}
